aed1151d586caa85d0b6d7012f2cbfafd0f9c545
Joseph E. Gonzalez <Joseph E. Gonzalez@localhost>
2012-06-07 15:50:57 -0400

    Dropping unsupported functionality in key user facing interfaces.

---
+++ extapis/cgi/demoapps/shortest_path/shortest_path.cpp
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <sstream>
+#include <graphlab.hpp>
+
+#include <graphlab/macros_def.hpp>
+#include "../../rapidjson.hpp"
+
+#define INITIAL_LENGTH 256
+
+namespace json = rapidjson;
+
+double stof(const char *str){
+  if (0 == strlen(str)) return 1e+99;
+  return atof(str);
+}
+
+void shortest_path_update(json::Document& invocation, json::Document& return_json){
+
+  json::Document::AllocatorType& allocator = return_json.GetAllocator();
+  return_json.SetObject();
+
+  // TODO: error handling for missing elements
+  const char *vertex_state = invocation["params"]["context"]["vertex"]["state"].GetString();
+  double vertex_dist = stof(vertex_state);
+  
+  // relax all incoming edges
+  json::Value& in_edges = invocation["params"]["context"]["in_edges"];
+  for (json::SizeType i = 0; i < in_edges.Size(); i++){
+    const json::Value& edge = in_edges[i];
+    double edge_dist = atof(edge["state"].GetString());
+    double nbr_dist = stof(edge["source"]["state"].GetString());
+    vertex_dist = std::min(vertex_dist, nbr_dist + edge_dist);
+  }
+  
+  std::ostringstream double_stream;
+  double_stream << vertex_dist << std::flush;
+  const char *double_str = double_stream.str().c_str();
+  
+  // add shortest distance to return json
+  json::Value vertex(double_str, allocator);
+  return_json.AddMember("vertex", vertex, allocator);
+  
+  json::Value vertices;
+  vertices.SetArray();
+  
+  // schedule affected members
+  const json::Value& out_edges = invocation["params"]["context"]["out_edges"];
+  for (json::SizeType i = 0; i < out_edges.Size(); i++){
+  
+    const json::Value& edge = out_edges[i];
+    int nbr_id = edge["target"]["id"].GetInt();
+    double nbr_dist = stof(edge["target"]["state"].GetString());
+    double edge_dist = atof(edge["state"].GetString());
+   
+     if (nbr_dist > (vertex_dist + edge_dist))
+       vertices.PushBack(nbr_id, allocator);
+    
+  }
+  
+  // add to return json
+  json::Value schedule;
+  schedule.SetObject();
+  schedule.AddMember("updater", "self", allocator);
+  schedule.AddMember("vertices", vertices, allocator);
+  return_json.AddMember("schedule", schedule, allocator);
+
+}
+
+const char *handle_invocation(const char *buffer, json::StringBuffer& return_buffer){
+
+  if (NULL == buffer) return NULL;
+
+  json::Document invocation;
+  if (invocation.Parse<0>(buffer).HasParseError()){/* TODO: error handling */}
+  
+  // TODO: error handling for missing elements
+  if (!strcmp(invocation["method"].GetString(), "exit")) return NULL;
+  if (!strcmp(invocation["method"].GetString(), "update")){
+  
+    json::Document return_json;
+    shortest_path_update(invocation, return_json);
+    
+    return_buffer.Clear();
+    json::Writer<json::StringBuffer> writer(return_buffer);
+    return_json.Accept(writer);
+    
+    return return_buffer.GetString();
+    
+  }
+
+  // TODO: error handling - method not found?
+  return NULL;
+  
+}
+
+int main(int argc, char** argv) {
+
+  std::string line;
+  std::size_t length = 0;
+  std::size_t current_length = INITIAL_LENGTH;
+  char *buffer = new char[current_length];
+  
+  // loop until exit is received
+  while (true){
+    
+    // TODO: assume NULL-terminated string for now
+    std::cin >> length;
+    std::getline(std::cin, line);
+    if (length + 1 > current_length){
+      current_length = length + 1;
+      delete[] buffer;
+      buffer = new char[current_length];
+    }
+    
+    // read message, break if exit
+    std::cin.read(buffer, length);
+    buffer[length] = NULL;    // terminate string w. null
+    json::StringBuffer return_buffer;
+    
+    const char *return_json = handle_invocation(buffer, return_buffer);
+    if (!return_json) break;
+    
+    // return
+    std::cout << strlen(return_json) << "\n";
+    std::cout << return_json << std::flush;
+    
+  }
+  
+  delete[] buffer;
+  return 0;
+
+}
---
+++ toolkits/matrix_factorization/als_vertex_program.hpp
@@ -0,0 +1,154 @@
+/**  
+ * Copyright (c) 2009 Carnegie Mellon University. 
+ *     All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS
+ *  IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied.  See the License for the specific language
+ *  governing permissions and limitations under the License.
+ *
+ * For more about this software visit:
+ *
+ *      http://www.graphlab.ml.cmu.edu
+ *
+ */
+
+#ifndef ALS_VERTEX_PROGRAM_HPP
+#define ALS_VERTEX_PROGRAM_HPP
+
+
+#include <Eigen/Dense>
+
+#include <graphlab.hpp>
+
+
+/** 
+ * \brief the vertex data type which contains the latent factor.
+ *
+ */
+struct vertex_data {
+  /**
+   * \brief A shared "constant" that specifies the number of latent
+   * values to use.
+   */
+  static size_t NLATENT;
+
+  /**
+   * \brief 
+   */
+  uint32_t nupdates; //! the number of times the vertex was updated
+  float residual; //! how much the latent value has changed
+  Eigen::VectorXd latent; //! vector of learned values 
+  vertex_data(); 
+  void randomize();
+  void save(graphlab::oarchive& arc) const;
+  void load(graphlab::iarchive& arc);
+}; // end of vertex data
+
+/**
+ * The edge data is just an observation float
+ */
+struct edge_data : public graphlab::IS_POD_TYPE {
+  float obs;
+  float error;
+  uint32_t nupdates;
+  edge_data(const float& obs = 0);
+}; // end of edge data
+
+
+/**
+ * The graph type
+ */ 
+typedef graphlab::distributed_graph<vertex_data, edge_data> graph_type;
+
+
+/**
+ * The gather type used to construct XtX and Xty needed for the ALS
+ * update
+ */
+class gather_type {
+public:
+  Eigen::MatrixXd XtX;
+  Eigen::VectorXd Xy;
+  gather_type() { }
+  gather_type(const Eigen::VectorXd& X, const double y);
+  void save(graphlab::oarchive& arc) const;
+  void load(graphlab::iarchive& arc);
+  gather_type& operator+=(const gather_type& other);
+}; // end of gather type
+
+
+/**
+ * ALS vertex program type
+ */ 
+class als_vertex_program : 
+  public graphlab::ivertex_program<graph_type, gather_type,
+                                   graphlab::messages::sum_priority>,
+  public graphlab::IS_POD_TYPE {
+public:
+  /** The convergence tolerance */
+  static double TOLERANCE;
+  static double LAMBDA;
+  static size_t MAX_UPDATES;
+
+  /** Initialize the vertex data */
+  void init(icontext_type& context, vertex_type& vertex);
+
+  /** The set of edges to gather along */
+  edge_dir_type gather_edges(icontext_type& context, 
+                             const vertex_type& vertex) const;
+
+  /** The gather function computes XtX and Xy */
+  gather_type gather(icontext_type& context, const vertex_type& vertex, 
+                     edge_type& edge) const;
+
+  /** apply collects the sum of XtX and Xy */
+  void apply(icontext_type& context, vertex_type& vertex,
+             const gather_type& sum);
+  
+  /** The edges to scatter along */
+  edge_dir_type scatter_edges(icontext_type& context,
+                              const vertex_type& vertex) const;
+
+  /** Scatter reschedules neighbors */  
+  void scatter(icontext_type& context, const vertex_type& vertex, 
+               edge_type& edge) const;
+  
+private:
+  /** Since the edges are undirected we use this helper function to
+      get the vertex on the other side of the edge */
+  vertex_type get_other_vertex(edge_type& edge, const vertex_type& vertex) const;
+
+}; // end of als vertex program
+
+
+
+
+
+//=============================================================================
+// Graph operations 
+
+
+/**
+ *  the extract error function is used to compute the error on an edge
+ */
+double extract_error(graph_type::edge_type edge);
+
+
+/**
+ * The graph loader function is a line parser used for distributed
+ * graph construction.
+ */
+bool graph_loader(graph_type& graph, 
+                  const std::string& filename,
+                  const std::string& line);
+
+
+#endif
