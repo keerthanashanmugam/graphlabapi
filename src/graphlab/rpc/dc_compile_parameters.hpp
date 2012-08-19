16df3daa7f20f5e5736adfc3f08d508b21fd2bae
Yucheng Low <Yucheng Low@localhost>
2012-08-19 02:36:46 -0400

    - Removed non-functional tracepoints

---
+++ src/graphlab/rpc/dc_compile_parameters.hpp
@@ -0,0 +1,24 @@
+#ifndef GRAPHLAB_DC_COMPILE_PARAMETERS_HPP
+#define GRAPHLAB_DC_COMPILE_PARAMETERS_HPP
+/** 
+  \ingroup rpc
+  \def RPC_DEFAULT_NUMHANDLERTHREADS
+  \brief default number of handler threads to spawn.
+ */ 
+#define RPC_DEFAULT_NUMHANDLERTHREADS 8
+
+/** 
+  \ingroup rpc
+  \def RPC_DEFAULT_COMMTYPE
+  \brief default communication method 
+ */ 
+#define RPC_DEFAULT_COMMTYPE TCP_COMM
+
+/** 
+  \ingroup rpc
+  \def RPC_MAX_N_PROCS
+  \brief Maximum number of processes supported 
+ */ 
+#define RPC_MAX_N_PROCS 128
+
+#endif
